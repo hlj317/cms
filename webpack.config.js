b002abdffc8aeb2c76051579d541df844a900a69
lijun.huang <huanglijun317@126.com>
2018-07-18 11:44:11 +0800

    开启http服务

--- webpack.config.js
+++ webpack.config.js
@@ -54,11 +54,11 @@ module.exports = (options = {}) => ({
     }
   },
   devServer: {
-    host: '127.0.0.1',
+    host: '0.0.0.0',
     port: 8010,
     proxy: {
       '/api/': {
-        target: 'http://127.0.0.1:8080',
+        target: 'http://0.0.0.0:8010',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
